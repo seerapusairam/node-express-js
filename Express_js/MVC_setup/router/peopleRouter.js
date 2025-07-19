7b8c83f653498bfa9cfff0d3b60d0f2b1d47ba3a
seerapusairam <seerapu.sai123@gmail.com>
2025-07-19 18:18:58 -0400

    router another method

--- Express_js/MVC_setup/router/peopleRouter.js
+++ Express_js/MVC_setup/router/peopleRouter.js
@@ -3,17 +3,23 @@ const router = express.Router()
 
 const controller = require('../controller/peopleController')
 
-//GET
-router.get('/', controller.getPeople)
-//POST
-router.post('/', controller.postPeople)
+// //GET
+// router.get('/', controller.getPeople)
+// //POST
+// router.post('/', controller.postPeople)
 
-//request to check in postman
-router.post('/postman', controller.postPostman)
+// //request to check in postman
+// router.post('/postman', controller.postPostman)
 
-//PUT - Update id
-router.put('/:id', controller.putPeople)
-//DELETE - id
-router.delete('/:id', controller.deletePeople)
+// //PUT - Update id
+// router.put('/:id', controller.putPeople)
+// // //DELETE - id
+// router.delete('/:id', controller.deletePeople)
+
+// or we can do in this method also 
+router.route('/').get(controller.getPeople).post(controller.postPeople)
+router.route('/postman').post(controller.postPostman)
+router.route('/:id').put(controller.putPeople).delete(controller.deletePeople)
+
 
 module.exports = router
