1c780e95f0d51416b8c850c40f84b0b8439ea575
seerapusairam <seerapu.sai123@gmail.com>
2025-07-28 18:40:01 -0400

    Db chaining

--- Store_API/controllers/products.js
+++ Store_API/controllers/products.js
@@ -29,13 +29,19 @@ const getAllProducts = async (req,res)=>{
         const fieldList = field.split(',').join(' ')
         result = result.select(fieldList)
     }
+    //Skip and limit
+    const limit = Number(req.query.limit) || 10
+    const page = Number(req.query.page) || 1
+    const skip = (page -1)*10
+
+    result = result.skip(skip).limit(limit)
      // Execute query
     const task = await result
     res.json({task,hits:task.length})
 }
 
 const getAllStatic = async (req,res)=>{
-    const allProducts = await model.find().select('name price')
+    const allProducts = await model.find().sort('name').select('name price').limit(5).skip(2) //chaining
     res.json({allProducts,hits:allProducts.length})
 }
 
