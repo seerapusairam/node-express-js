36e335865e9045b5bd367227cab6c54b2092dcca
seerapusairam <seerapu.sai123@gmail.com>
2025-07-30 17:49:58 -0400

    Added JWT auth

---
+++ JWT Auth/controllers/main.js
@@ -0,0 +1,28 @@
+const customError = require('../errors/custom-error')
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
+const secret = process.env.JWT_SECRET
+
+const dashboard = async(req,res,next)=>{
+    const randomNumber = Math.floor(Math.random()*100)
+    res.status(200).json({msg:"Hello",secret:`your lucky number ${randomNumber}`})
+}
+
+const login = async(req,res,next)=>{
+    const {username,password} = req.body
+
+    if(!username || !password){
+        throw new customError("Please Provide the data",400)
+    }
+
+    //just for demo
+    const id = new Date().getDate()
+
+    const token = jwt.sign({id,username},secret,{expiresIn:'30d'})
+    res.json({message:"Fine",token})
+
+}
+
+module.exports = {
+    dashboard,login
+}
